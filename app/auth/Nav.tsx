7fdccbb81b6b873571f0628ec45b49c3b37c2fd5
APatel-AI <arpan.p.se@gmail.com>
2023-11-09 12:43:21 -0600

    add: signout feature, auto render profile image.

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
 import Login from "./Login";
-
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import Logged from "./Logged";
 export default async function Nav() {
-    return(
-        <nav className="flex justify-between items-center py-8"> 
-            <Link href={"/"}>
-                <h1 className="font-bold text-lg ">Send it.</h1>
-                {/* Client Component */}
+  const session = await getServerSession(authOptions);
+  // Test
+//   console.log(session);
 
-            </Link>   
+  return (
+    <nav className="flex justify-between items-center py-8">
+      <Link href={"/"}>
+        <h1 className="font-bold text-lg ">Send it.</h1>
+        {/* Client Component */}
+      </Link>
 
-            <ul className="flex items-center gap-6">
-                <Login />
-            </ul>
-        </nav>
-    )
-    
+      <ul className="flex items-center gap-6">
+        {/* Only show login screen if there is no user signed in */}
+        {!session?.user && <Login/>}
+        {/* If the user is logged in: */}
+        {session?.user && <Logged image={session.user?.image || ""}/> }
+      </ul>
+    </nav>
+  );
 }
